a0f51f012f3a48093d4848a42255649fd7f6faa8
rafiandria23 <rafiandria23@gmail.com>
2020-06-15 20:09:38 +0700

    Removed old src from Vue.js as the impact of the migration.

--- src2/utils/colors.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  error: "#F44336",
-  success: "#4CAF50",
-  info: "rgb(7, 121, 228)"
-};
--- src2/views/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as HomePage } from "@/views/HomePage.vue";
-export { default as AboutPage } from "@/views/AboutPage.vue";
-export { default as Dashboard } from "@/views/Dashboard.vue";
