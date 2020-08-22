27466814cb4a1e7c439272f6e1d4ccaa08f424e1
Adam Rafiandri <rafiandria23@gmail.com>
2020-08-22 14:41:32 +0700

    UPDATE: Reconfigured Firebase functions and hosting.

---
+++ firebase-functions.js
@@ -0,0 +1,19 @@
+const { join } = require('path');
+const { https } = require('firebase-functions');
+const { default: next } = require('next');
+
+const isDev = process.env.NODE_ENV !== 'production';
+const nextjsDistDir = join(require('./next.config').distDir);
+
+const nextjsServer = next({
+  dev: isDev,
+  conf: {
+    distDir: nextjsDistDir,
+  },
+});
+
+const nextjsHandle = nextjsServer.getRequestHandler();
+
+exports.nextjsFunc = https.onRequest((req, res) =>
+  nextjsServer.prepare().then(() => nextjsHandle(req, res)),
+);
